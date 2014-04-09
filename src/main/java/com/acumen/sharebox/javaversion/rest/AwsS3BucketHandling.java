c22e39bca79fd796d060f09b116fe03b275d7b26
Rohan <rohan.tan@gmail.com>
2014-04-09 13:53:24 -0700

    base version commit
    file upload rest API

---
+++ src/main/java/com/acumen/sharebox/javaversion/rest/AwsS3BucketHandling.java
@@ -0,0 +1,78 @@
+package com.acumen.sharebox.javaversion.rest;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.RestoreObjectRequest;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.transfer.TransferManager;
+
+public class AwsS3BucketHandling {
+	
+	public String addS3BucketObjects( File fileobject,String key){
+		String response="fail";
+		AmazonS3 s3 = new AmazonS3Client(new ClasspathPropertiesFileCredentialsProvider());
+		Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+		s3.setRegion(usWest2);
+		String bucketName = "shareboxbucket";
+
+		System.out.println("===========================================");
+		System.out.println("Getting Started with Amazon S3");
+		System.out.println("===========================================\n");
+
+		try {
+			System.out.println("Listing buckets");
+
+			System.out.println("\nUploading a new object to S3...");
+			System.out.println("upload file name:: "+key);
+			s3.putObject(new PutObjectRequest(bucketName, key, fileobject));
+			System.out.println("File uploaded on S3 - location: "+bucketName+" -> "+key);
+			response="success";
+
+		} catch (AmazonServiceException ase) {
+			System.out.println("Caught an AmazonServiceException, which means your request made it "
+					+ "to Amazon S3, but was rejected with an error response for some reason.");
+			System.out.println("Error Message:    " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code:   " + ase.getErrorCode());
+			System.out.println("Error Type:       " + ase.getErrorType());
+			System.out.println("Request ID:       " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out.println("Caught an AmazonClientException, which means the client encountered "
+					+ "a serious internal problem while trying to communicate with S3, "
+					+ "such as not being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}
+		return response;
+	}
+	
+	
+	public static void main(String args[]){
+		//AwsS3BucketHandling ws=new AwsS3BucketHandling();
+		//String res=ws.viewS3BucketObjects();
+		//System.out.println(res);
+//		File  directory=new File("E:\\S3UploadFiles");
+//		String response=ws.addFolderS3BucketObjects(directory,directory.getName());
+//		String response=ws.deleteS3BucketObjects("python_tutorial.pdf");
+//		String response=ws.getS3BucketObjects("E:\\S3DownloadFiles\\");
+//		String response=ws.restoreS3BucketObjects("cmpe273-lec6-REST.pdf",2);
+//		System.out.println("response: "+response);
+	}
+	
+}
---
+++ src/main/java/com/acumen/sharebox/javaversion/rest/FileHandling.java
@@ -0,0 +1,37 @@
+package com.acumen.sharebox.javaversion.rest;
+
+import java.io.File;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+@Path("files")
+public class FileHandling{
+
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") File fileobject,
+            @FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
+    	System.out.println("inside class uploadFile###################");
+    	AwsS3BucketHandling awsS3BucketHandling=new AwsS3BucketHandling();
+    	System.out.println("filename: $$$$ "+contentDispositionHeader.getFileName());
+    	String output=awsS3BucketHandling.addS3BucketObjects(fileobject,contentDispositionHeader.getFileName());
+        return Response.status(200).entity(output).build();
+    }  
+    
+    
+}
