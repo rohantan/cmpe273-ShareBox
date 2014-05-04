3e5d377cae12e199f779f06b2a8dfc732221468d
Rohan <rohan.tan@gmail.com>
2014-05-04 16:14:37 -0700

    home page table-changes
    included all functionality for table created on home page

--- WebContent/js/loadTable.js
+++ WebContent/js/loadTable.js
@@ -1,14 +1,14 @@
 function changeImportance(value) {
+	
 	var urlrate = value.id + value.value;
-//	alert(urlrate);
 	$.ajax({
-		type : "GET",
+		type : "POST",
 		url : urlrate,
 		success : function(msg) {
-//			alert("File rated" + msg);
+			
 		},
 		error : function() {
-//			alert("Error");
+			alert("Error");
 		}
 	});
 	var color="ui-widget-content";
@@ -25,20 +25,34 @@ function changeImportance(value) {
 	color="ui-widget-content";
 }
 
+function redirectViewLogs(){
+	onLoadFunction();
+	location.href='ViewLogs.html';
+	window.location(location.href);
+}
 
-function deleteFile() {
-	var url = event.target.getAttribute("id");
-//	alert(emailid);
-//	alert(url);
+function redirectSharedByMeFiles(){
+	onLoadFunction();
+	location.href='SharedByMeFiles.html';
+	window.location(location.href);
+}
+
+function redirectSharedFiles(){
+	onLoadFunction();
+	location.href='SharedFiles.html';
+	window.location(location.href);
+}
+
+function deleteFile(value) {
+	var url = value.id;
 	$.ajax({
 		type: "DELETE",
 		url: url,
 		success: function(msg){
-//			alert("File Deleted"+msg);
 			onLoadFunction();
 		},
 		error: function () {
-//			alert("Error");
+			alert("Error");
 		}
 
 	});
@@ -51,54 +65,37 @@ function logoutfunction(){
 }
 
 function shareFile(value){
+	onLoadFunction();
 	sessionStorage.shareURL=value.id;
-//	alert("sessionStorage.shareURL"+sessionStorage.shareURL);
 	location.href='shareFile.html';
 	window.location(location.href);
 }
 
 function viewimghere(value){
-//	alert("in img");
-//	alert(value.id);
+	onLoadFunction();
 	sessionStorage.linkval=value.id;
 	location.href='viewImgFileHere.html';
 	window.location(location.href);
 }
 function viewvideohere(value){
-//	alert("in video");
-//	alert(value.id);
+	onLoadFunction();
 	sessionStorage.linkval=value.id;
+	sessionStorage.tempval=sessionStorage.linkval;
 	location.href='viewVideoFileHere.html';
 	window.location(location.href);
 }
 
 function sharefilerequest(){
+//	onLoadFunction();
 	var serviceurl=sessionStorage.shareURL;
-//	alert(serviceurl+document.getElementById("recemailid").value);
 	var Url=serviceurl+document.getElementById("recemailid").value;
 	$.ajax({
 		type: "POST",
 		url: Url,
 		success: function(msg){
-//			alert("File shared: "+msg);
 			sessionStorage.shareURL='';
-			$("#myDialogText").text("Your file has been shared with "+document.getElementById("recemailid").value);
-			$("#dialog-message").dialog({
-			    modal: true,
-			    draggable: true,
-			    resizable: true,
-			    position: ['center', 'top'],
-			    visibility: true,
-			    width: 200,
-			    dialogClass: 'ui-dialog-osx',
-			    buttons: {
-			        "Okay": function() {
-			            $(this).dialog("close");
-			            location.href='shareboxHome.html';
-						window.location(location.href);
-			        }
-			    }
-			});
+			location.href='shareboxHome.html';
+			window.location(location.href);
 			
 		},
 		error: function () {
@@ -109,11 +106,14 @@ function sharefilerequest(){
 }
 
 function onLoadFunction(){
-//	alert("inside onload");
 	var color="ui-widget-content";
 	var optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
 	var emailid=sessionStorage.username;
-	var url= window.location.protocol + "//" + window.location.host  + "/ShareBox/rest/files"; 
+//	var url= window.location.protocol + "//" + window.location.host  + "/ShareBox/rest/files"; 
+	
+	//use this for server deployment
+	var url = window.location.protocol + "//" + window.location.hostname + "/rest/files";
+	
 	$.ajax({
 		type: "GET",
 		async:false,
@@ -137,57 +137,39 @@ function onLoadFunction(){
 				hrefViewHere=hrefViewHere.replace("\\", "");
 				
 				if(hrefViewHere.indexOf(".mp4")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewvideohere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else if(hrefViewHere.indexOf(".MP4")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewvideohere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else if(hrefViewHere.indexOf(".jpg")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewimghere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else if(hrefViewHere.indexOf(".JPG")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewimghere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else if(hrefViewHere.indexOf(".png")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewimghere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else if(hrefViewHere.indexOf(".PNG")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewimghere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else if(hrefViewHere.indexOf(".jpeg")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewimghere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}
 				else if(hrefViewHere.indexOf(".JPEG")!=-1){
-//					alert(hrefViewHere);
 					classval="class='bt-button-delete'";
 					onclickval="onclick='viewimghere(this)'";
-//					sessionStorage.linkvalue=hrefViewHere;
 					
 				}else{
-//					alert("disabled");
-//					alert(hrefViewHere);
 					disablity="disabled='disabled'";
 					classval="class='bt-button-viewhere'";
 					onclickval='';
@@ -209,17 +191,14 @@ function onLoadFunction(){
 					color="rate-color-green";
 					optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4" selected="selected">Very Imp</option>';
 				}
-				/*html1+='<tr class='+color+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td >'+obj.entries[i].rating+'</td><td ><button class="deleteFile bt-button-delete" id="'+hrefDelete+'">Delete</button></td></tr>';*/
-				html1+='<tr class='+color+' id='+obj.entries[i].filename+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td><select id="'+changeImp+'" onchange="changeImportance(this)" name='+obj.entries[i].filename+'>'+optionText+'</select></td><td ><button onclick="deleteFile()" class="bt-button-delete" id="'+hrefDelete+'">Delete</button></td><td ><button onclick="shareFile(this)" class="bt-button-delete" id="'+hrefShare+'">Share</button></td><td><button '+disablity+' '+onclickval+' id="'+hrefViewHere+'" '+classval+'>View</button></td></tr>';
+				html1+='<tr class='+color+' id='+obj.entries[i].filename+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td><select id="'+changeImp+'" onchange="changeImportance(this)" name='+obj.entries[i].filename+'>'+optionText+'</select></td><td ><button onclick="deleteFile(this)" class="bt-button-delete" id="'+hrefDelete+'">Delete</button></td><td ><button onclick="shareFile(this)" class="bt-button-delete" id="'+hrefShare+'">Share</button></td><td><button '+disablity+' '+onclickval+' id="'+hrefViewHere+'" '+classval+'>View</button></td></tr>';
 				hrefDelete='';
 				hrefDownload = '';
 				hrefShare='';
 				disablity='';
-//				classval='';
 				optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
 				color="ui-widget-content";
 			}
-			//$('#dataTable tbody').after(html1).trigger('create');
 			$('#dataTable tbody').html(html1);
 
 		},
@@ -230,18 +209,24 @@ function onLoadFunction(){
 }
 
 $( document ).ready(function() {
+//	alert("on ready");
 	var color="ui-widget-content";
 	var optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
 	var emailid=sessionStorage.username;
-	var url= window.location.protocol + "//" + window.location.host  + "/ShareBox/rest/files"; 
+//	var url= window.location.protocol + "//" + window.location.host  + "/ShareBox/rest/files"; 
+	
+	//use this for server deployment
+	var url = window.location.protocol + "//" + window.location.hostname + "/rest/files";
+	
 	onLoadFunction();
 	function onLoadFunction(){
-//		alert("inside onload");
+//		alert("on laod function sessionStorage.username"+sessionStorage.username);
 		$.ajax({
 			type: "GET",
 			async:false,
 			url: url+"/view/" +emailid,
 			success: function(msg){
+//				alert("in succsess: "+msg);
 				var obj = jQuery.parseJSON( ''+ msg +'' );
 				var html1= '';
 				var hrefDelete='';
@@ -260,57 +245,39 @@ $( document ).ready(function() {
 					hrefViewHere=hrefViewHere.replace("\\", "");
 					
 					if(hrefViewHere.indexOf(".mp4")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewvideohere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else if(hrefViewHere.indexOf(".MP4")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewvideohere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else if(hrefViewHere.indexOf(".jpg")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewimghere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else if(hrefViewHere.indexOf(".JPG")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewimghere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else if(hrefViewHere.indexOf(".png")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewimghere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else if(hrefViewHere.indexOf(".PNG")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewimghere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else if(hrefViewHere.indexOf(".jpeg")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewimghere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}
 					else if(hrefViewHere.indexOf(".JPEG")!=-1){
-//						alert(hrefViewHere);
 						classval="class='bt-button-delete'";
 						onclickval="onclick='viewimghere(this)'";
-//						sessionStorage.linkvalue=hrefViewHere;
 						
 					}else{
-//						alert("disabled");
-//						alert(hrefViewHere);
 						disablity="disabled='disabled'";
 						classval="class='bt-button-viewhere'";
 						onclickval='';
@@ -332,17 +299,15 @@ $( document ).ready(function() {
 						color="rate-color-green";
 						optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4" selected="selected">Very Imp</option>';
 					}
-					/*html1+='<tr class='+color+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td >'+obj.entries[i].rating+'</td><td ><button class="deleteFile bt-button-delete" id="'+hrefDelete+'">Delete</button></td></tr>';*/
-					html1+='<tr class='+color+' id='+obj.entries[i].filename+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td><select id="'+changeImp+'" onchange="changeImportance(this)" name='+obj.entries[i].filename+'>'+optionText+'</select></td><td ><button onclick="deleteFile()" class="bt-button-delete" id="'+hrefDelete+'">Delete</button></td><td ><button onclick="shareFile(this)" class="bt-button-delete" id="'+hrefShare+'">Share</button></td><td><button '+disablity+' '+onclickval+' id="'+hrefViewHere+'" '+classval+'>View</button></td></tr>';
+					html1+='<tr class='+color+' id='+obj.entries[i].filename+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td><select id="'+changeImp+'" onchange="changeImportance(this)" name='+obj.entries[i].filename+'>'+optionText+'</select></td><td ><button onclick="deleteFile(this)" class="bt-button-delete" id="'+hrefDelete+'">Delete</button></td><td ><button onclick="shareFile(this)" class="bt-button-delete" id="'+hrefShare+'">Share</button></td><td><button '+disablity+' '+onclickval+' id="'+hrefViewHere+'" '+classval+'>View</button></td></tr>';
 					hrefDelete='';
 					hrefDownload = '';
 					hrefShare='';
 					disablity='';
-//					classval='';
 					optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
 					color="ui-widget-content";
 				}
-				//$('#dataTable tbody').after(html1).trigger('create');
+//				alert(html1);
 				$('#dataTable tbody').html(html1);
 
 			},
