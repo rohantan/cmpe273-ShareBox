840100f933e74c7b2a3c2cb76a7f0dee0701b81c
preetiborade <preeti.borade@gmail.com>
2014-05-07 01:55:23 -0700

    Create loadSharedByMeToTable.js

---
+++ WebContent/loadSharedByMeToTable.js
@@ -0,0 +1,237 @@
+
+function viewimghere(value){
+	onLoadFunction();
+	sessionStorage.linkval=value.id;
+	location.href='viewImgFileHere.html';
+	window.location(location.href);
+}
+function viewvideohere(value){
+	onLoadFunction();
+	sessionStorage.linkval=value.id;
+	location.href='viewVideoFileHere.html';
+	window.location(location.href);
+}
+function viewaudiohere(value){
+	onLoadFunction();
+	sessionStorage.linkval=value.id;
+	sessionStorage.tempval=sessionStorage.linkval;
+	location.href='listenAudioFileHere.html';
+	window.location(location.href);
+}
+
+function onLoadFunction(){
+	var color="ui-widget-content";
+	var optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
+	var emailid=sessionStorage.username;
+//	var url= window.location.protocol + "//" + window.location.host  + "/ShareBox/rest/files"; 
+	
+	//use this for server deployment
+	var url = window.location.protocol + "//" + window.location.hostname + "/rest/files";
+	
+	$.ajax({
+		type: "GET",
+		async:false,
+		url: url+"/view/" +emailid,
+		success: function(msg){
+			var obj = jQuery.parseJSON( ''+ msg +'' );
+			var html1= '';
+			var hrefDelete='';
+			var hrefDownload='';
+			var hrefShare='';
+			var hrefViewHere='';
+			var disablity='';
+			var classval='';
+			var onclickval='';
+			for ( var i = 0; i < obj.entries.length; i++) {
+				hrefDownload = obj.entries[i].downloadlink;
+				hrefDelete = url+'/'+emailid+'/'+obj.entries[i].filename;
+				changeImp = url+"/rateFile/"+emailid+"/"+obj.entries[i].filename+"/";
+				hrefShare= url+"/share/"+emailid+"/"+obj.entries[i].filename+"/";
+				hrefViewHere=obj.entries[i].downloadlink;
+				hrefViewHere=hrefViewHere.replace("\\", "");
+				
+				if(hrefViewHere.indexOf(".mp4")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewvideohere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".MP4")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewvideohere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".jpg")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewimghere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".JPG")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewimghere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".png")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewimghere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".PNG")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewimghere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".jpeg")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewimghere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".JPEG")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewimghere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".mp3")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewaudiohere(this)'";
+					
+				}else if(hrefViewHere.indexOf(".MP3")!=-1){
+					classval="class='bt-button-delete'";
+					onclickval="onclick='viewaudiohere(this)'";
+					
+				}else{
+					disablity="disabled='disabled'";
+					classval="class='bt-button-viewhere'";
+					onclickval='';
+				}
+				
+				if(obj.entries[i].rating==1){
+					color="ui-widget-content";
+					optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
+				}
+				if(obj.entries[i].rating==2){
+					color="rate-color-pink";
+					optionText='<option value="1" selected="selected">No Idea</option><option value="2" selected="selected">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
+				}
+				if(obj.entries[i].rating==3){
+					color="rate-color-blue";
+					optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3" selected="selected">Moderate</option><option value="4">Very Imp</option>';
+				}
+				if(obj.entries[i].rating==4){
+					color="rate-color-green";
+					optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4" selected="selected">Very Imp</option>';
+				}
+				html1+='<tr class='+color+' id='+obj.entries[i].filename+'><td >'+obj.entries[i].filename+'</td><td >'+obj.entries[i].filesize+'</td><td >'+obj.entries[i].lastmodified+'</td><td ><a class="download_link" href="'+hrefDownload+'">Download</a></td><td><select id="'+changeImp+'" onchange="changeImportance(this)" name='+obj.entries[i].filename+'>'+optionText+'</select></td><td ><button onclick="deleteFile(this)" class="bt-button-delete" id="'+hrefDelete+'">Delete</button></td><td ><button onclick="shareFile(this)" class="bt-button-delete" id="'+hrefShare+'">Share</button></td><td><button '+disablity+' '+onclickval+' id="'+hrefViewHere+'" '+classval+'>View</button></td></tr>';
+				hrefDelete='';
+				hrefDownload = '';
+				hrefShare='';
+				disablity='';
+				optionText='<option value="1" selected="selected">No Idea</option><option value="2">Amber</option><option value="3">Moderate</option><option value="4">Very Imp</option>';
+				color="ui-widget-content";
+			}
+			$('#dataTable tbody').html(html1);
+
+		},
+		error: function () {
+			alert("Error");
+		}
+	});
+}
+
+
+
+$( document ).ready(function() {
+	var emailid=sessionStorage.username;
+	
+//	var url= window.location.protocol + "//" + window.location.host  + "/ShareBox/rest/files"; 
+	
+	
+	//use this for server deployment
+	var url = window.location.protocol + "//" + window.location.hostname + "/rest/files";
+	
+	
+	onLoadFunction();
+	function onLoadFunction(){
+//		alert("inside onload");
+		$.ajax({
+			type: "GET",
+			async:false,
+			url: url+"/viewsharedbyme/" +emailid,
+			success: function(msg){
+				var obj = jQuery.parseJSON( ''+ msg +'' );
+				var html1= '';
+				var hrefViewHere='';
+				var disablity='';
+				var classval='';
+				var onclickval='';
+				for ( var i = 0; i < obj.sharedfiles.length; i++) {
+					hrefViewHere=obj.sharedfiles[i].downloadlink;
+					hrefViewHere=hrefViewHere.replace("\\", "");
+					if(hrefViewHere.indexOf(".mp4")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewvideohere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".MP4")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewvideohere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".jpg")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewimghere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".JPG")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewimghere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".png")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewimghere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".PNG")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewimghere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".jpeg")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewimghere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".JPEG")!=-1){
+//						alert(hrefViewHere);
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewimghere(this)'";
+//						sessionStorage.linkvalue=hrefViewHere;
+						
+					}else if(hrefViewHere.indexOf(".mp3")!=-1){
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewaudiohere(this)'";
+						
+					}else if(hrefViewHere.indexOf(".MP3")!=-1){
+						classval="class='bt-button-delete'";
+						onclickval="onclick='viewaudiohere(this)'";
+						
+					}else{
+//						alert("disabled");
+//						alert(hrefViewHere);
+						disablity="disabled='disabled'";
+						classval="class='bt-button-viewhere'";
+						onclickval='';
+					}
+					html1+='<tr class="ui-widget-content" id='+obj.sharedfiles[i].filename+'><td >'+obj.sharedfiles[i].filename+'</td><td >'+obj.sharedfiles[i].filesize+'</td><td >'+obj.sharedfiles[i].receiver+'</td><td><button '+disablity+' '+onclickval+' id="'+hrefViewHere+'" '+classval+'>View</button></td></tr>';
+					hrefDelete='';
+					hrefDownload = '';
+					disablity='';
+				}
+				$('#dataTable tbody').html(html1);
+			},
+			error: function () {
+				alert("Error");
+			}
+		});
+	}
+	
+});
